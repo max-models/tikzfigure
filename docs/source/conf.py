a00d458cd26585a147e0a89e86da070fa90b572b
Max <max.lindqvist@ipp.mpg.de>
2025-09-14 10:32:12 +0200

    formatting and bugfix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,8 +5,10 @@
 
 import os
 import shutil
+
 # At the top.
 import sphinx_bootstrap_theme
+
 
 def copy_tutorials(app):
     src = os.path.abspath("../tutorials")
@@ -58,7 +60,7 @@ source_suffix = {
 # ...
 
 # Activate the theme.
-html_theme = 'bootstrap'
+html_theme = "bootstrap"
 html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # (Optional) Logo. Should be small enough to fit the navbar (ideally 24x24).
@@ -69,12 +71,9 @@ html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 # theme further.
 html_theme_options = {
     # Navigation bar title. (Default: ``project`` value)
-    'navbar_title': "python-template",
-
+    "navbar_title": "python-template",
     # Tab name for entire site. (Default: "Site")
-    
-    'navbar_site_name': "Site",
-
+    "navbar_site_name": "Site",
     # A list of tuples containing pages or urls to link to.
     # Valid tuples should be in the following forms:
     #    (name, page)                 # a link to a page
@@ -82,26 +81,21 @@ html_theme_options = {
     #    (name, "http://example.com", True) # arbitrary absolute url
     # Note the "1" or "True" value above as the third argument to indicate
     # an arbitrary url.
-    'navbar_links': [
+    "navbar_links": [
         ("Quickstart", "quickstart"),
-        ("Tutorials", "tutorial"),
+        ("Tutorials", "tutorials"),
         ("API", "api"),
         # ("Link", "http://example.com", True),
     ],
-
     # Render the next and previous page links in navbar. (Default: true)
-    'navbar_sidebarrel': False,
-
+    "navbar_sidebarrel": False,
     # Render the current pages TOC in the navbar. (Default: true)
-    'navbar_pagenav': False,
-
+    "navbar_pagenav": False,
     # Tab name for the current pages TOC. (Default: "Page")
-    'navbar_pagenav_name': "Page",
-
+    "navbar_pagenav_name": "Page",
     # Global TOC depth for "site" navbar tab. (Default: 1)
     # Switching to -1 shows all levels.
-    'globaltoc_depth': 2,
-
+    "globaltoc_depth": 2,
     # Include hidden TOCs in Site navbar?
     #
     # Note: If this is "false", you cannot have mixed ``:hidden:`` and
@@ -109,20 +103,16 @@ html_theme_options = {
     # will break.
     #
     # Values: "true" (default) or "false"
-    'globaltoc_includehidden': "true",
-
+    "globaltoc_includehidden": "true",
     # HTML navbar class (Default: "navbar") to attach to <div> element.
     # For black navbar, do "navbar navbar-inverse"
-    'navbar_class': "navbar inverse",
-
+    "navbar_class": "navbar inverse",
     # Fix navigation bar to top of page?
     # Values: "true" (default) or "false"
-    'navbar_fixed_top': "true",
-
+    "navbar_fixed_top": "true",
     # Location of link to source.
     # Options are "nav" (default), "footer" or anything else to exclude.
-    'source_link_position': "footer",
-
+    "source_link_position": "footer",
     # Bootswatch (http://bootswatch.com/) theme.
     #
     # Options are nothing (default) or the name of a valid theme
@@ -134,17 +124,14 @@ html_theme_options = {
     # Currently, the supported themes are:
     # - Bootstrap 2: https://bootswatch.com/2
     # - Bootstrap 3: https://bootswatch.com/3
-    'bootswatch_theme': "flatly",
-
+    "bootswatch_theme": "flatly",
     # Choose Bootstrap version.
     # Values: "3" (default) or "2" (in quotes)
-    'bootstrap_version': "3",
+    "bootstrap_version": "3",
 }
 
 html_static_path = ["_static"]
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 
-html_sidebars = {
-    '**': []
-}
+html_sidebars = {"**": []}
