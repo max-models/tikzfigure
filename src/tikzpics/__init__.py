b325a0d5d2477f18fef44cfd27f6666d01251139
Max <max.lindqvist@ipp.mpg.de>
2026-02-03 18:23:30 +0100

    Added the load magic to the __init__.py

--- src/tikzpics/__init__.py
+++ src/tikzpics/__init__.py
@@ -2,3 +2,17 @@ from tikzpics.figure import TikzFigure
 from tikzpics.node import Node
 
 __all__ = ["TikzFigure", "Node"]
+
+
+def load_ipython_extension(ipython):
+    """Load the IPython magic extension."""
+    from tikzpics.ipython import load_ipython_extension as _load
+
+    _load(ipython)
+
+
+def unload_ipython_extension(ipython):
+    """Unload the IPython magic extension."""
+    from tikzpics.ipython import unload_ipython_extension as _unload
+
+    _unload(ipython)
