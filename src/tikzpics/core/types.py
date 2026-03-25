1ac285ce640d926f9f127208b59dada03caacf2d
Max <max.lindqvist@ipp.mpg.de>
2026-03-25 20:06:03 +0100

    Add remaining draw args (#89)
    * Added remaining arguments for nodes and draw
    
    * Added literals
    
    * Move literals to types.py

---
+++ src/tikzpics/core/types.py
@@ -0,0 +1,122 @@
+from typing import Literal
+
+# ---------- Literal type aliases for IDE autocomplete ---------- #
+
+_Shape = (
+    Literal[
+        "circle",
+        "rectangle",
+        "diamond",
+        "ellipse",
+        "star",
+        "regular polygon",
+        "trapezium",
+        "semicircle",
+        "cylinder",
+        "dart",
+        "kite",
+        "isosceles triangle",
+        "signal",
+        "cloud",
+        "forbidden sign",
+        "cross out",
+        "strike out",
+    ]
+    | str
+    | None
+)
+
+_Anchor = (
+    Literal[
+        "center",
+        "north",
+        "south",
+        "east",
+        "west",
+        "north east",
+        "north west",
+        "south east",
+        "south west",
+        "base",
+        "mid",
+        "base east",
+        "base west",
+        "mid east",
+        "mid west",
+    ]
+    | str
+    | None
+)
+
+_LineCap = Literal["butt", "rect", "round"] | str | None
+_LineJoin = Literal["miter", "bevel", "round"] | str | None
+_Align = Literal["left", "center", "right", "justify"] | str | None
+_Shading = Literal["axis", "radial", "ball"] | str | None
+
+_Pattern = (
+    Literal[
+        "horizontal lines",
+        "vertical lines",
+        "north east lines",
+        "north west lines",
+        "grid",
+        "crosshatch",
+        "dots",
+        "crosshatch dots",
+        "fivepointed stars",
+        "sixpointed stars",
+        "bricks",
+        "checkerboard",
+    ]
+    | str
+    | None
+)
+
+_Mark = (
+    Literal[
+        "*",
+        "x",
+        "o",
+        "+",
+        "|",
+        "-",
+        "square",
+        "square*",
+        "triangle",
+        "triangle*",
+        "diamond",
+        "diamond*",
+        "pentagon",
+        "pentagon*",
+        "oplus",
+        "oplus*",
+        "otimes",
+        "otimes*",
+        "asterisk",
+        "star",
+        "10-pointed star",
+        "ball",
+    ]
+    | str
+    | None
+)
+
+_Decoration = (
+    Literal[
+        "zigzag",
+        "snake",
+        "coil",
+        "bumps",
+        "bent",
+        "random steps",
+        "saw",
+        "brace",
+        "ticks",
+        "border",
+        "markings",
+        "expanding waves",
+        "footprints",
+    ]
+    | str
+    | None
+)
