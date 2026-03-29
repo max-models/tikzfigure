251ea192e12e4213fa6de3b5eb01423eccf70c07
Max <max.lindqvist@ipp.mpg.de>
2026-03-29 12:38:54 +0200

    Format math with katex

--- astro_docs/astro.config.mjs
+++ astro_docs/astro.config.mjs
@@ -1,14 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://max-models.github.io/tikzpics',
 	base: '/tikzpics',
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex],
+	},
 	integrations: [
 		starlight({
 			title: 'tikzpics',
+			customCss: ['katex/dist/katex.min.css'],
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/max-models/tikzpics' }],
 			sidebar: [
 				{ label: 'Home', slug: '' },
