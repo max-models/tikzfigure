a7ab8f6a5afe4f9705959c3f3432d6e1ea5be705
Max <max.lindqvist@ipp.mpg.de>
2026-03-29 09:33:52 +0200

    Cleanup tutorials pages (#101)
    * Remove double spaces in TikzFigure.from_tikz_code()
    
    * Converted tutorials to qmd
    
    * Install quarto first
    
    * Install jq
    
    * Removed sudo
    
    * install quarto in build docs
    
    * Install jq and wget
    
    * Pushed docker image, use it in the CI
    
    * Added credentails (should not be needed)
    
    * Removed tutorials/tutorial_11_jupyter_magic.qmd
    
    * Added tutorials/tutorial_11_jupyter_magic.ipynb
    
    * Added copy of tutorials/tutorial_11_jupyter_magic.ipynb to the build directory
    
    * Updated dockerfile instructions to build with linux/amd64
    
    * Run CI

--- docs/hooks.py
+++ docs/hooks.py
@@ -1,11 +1,26 @@
-import shutil
+import subprocess
 from pathlib import Path
 
 
 def on_pre_build(config):
+    import shutil
+
     tutorials_src = Path("tutorials")
     tutorials_dst = Path("docs/tutorials")
     if tutorials_src.exists():
         tutorials_dst.mkdir(exist_ok=True)
+        for qmd in tutorials_src.glob("*.qmd"):
+            subprocess.run(
+                [
+                    "quarto",
+                    "render",
+                    str(qmd),
+                    "--to",
+                    "gfm",
+                    "--output-dir",
+                    str(tutorials_dst.resolve()),
+                ],
+                check=True,
+            )
         for nb in tutorials_src.glob("*.ipynb"):
-            shutil.copy2(nb, tutorials_dst / nb.name)
+            shutil.copy(nb, tutorials_dst / nb.name)
